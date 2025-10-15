570e2fb38c3f5c51ef20f2a20f5bb932dc653ab9
Soni Panta <sonipanta8@gmail.com>
2025-10-15 16:42:46 +0545

    generating username from email
    Generating username and domain from email address using substring();

---
+++ email.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class First{
+    public static void main(String[] args){
+      
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter your email address:");
+        String email=scanner.nextLine();
+        if(email.isEmpty()){
+            System.out.println("Email address cant be empty");
+
+        }
+        else if(email.contains(" ")){
+            System.out.println("Email address must not contain space");
+
+        }
+        else if(!email.contains(".")){
+            System.out.println("Email address must contain atleast one '.'");
+
+        }
+        else if(!email.contains("@")){
+            System.out.println("Email address must contain @");
+            
+        }
+        else{
+            System.out.println("Username:"+email.substring(0,email.indexOf("@")));
+            System.out.println("Domain:"+email.substring(email.indexOf("@")));
+        }
+
+    }
+}
